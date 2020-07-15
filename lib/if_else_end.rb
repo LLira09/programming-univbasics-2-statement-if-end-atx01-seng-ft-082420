a2887ccc7db9c8484afb4fd70834c90752e51555
Jake <llira09@gmail.com>
2020-07-15 21:03:57 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -26,3 +26,14 @@ chance_of_rain = 0.9
 # puts "You know what year it is??"
 # this_year = 2019
 # puts "Hey, it's not 2019" unless this_year == 2019
+
+current_time = Time.now
+current_time = current_time.to_i
+if current_time % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
+
+
+
